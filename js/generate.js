d60d54060b30cb3eb8b1729aa7001cb82bec24e9
Then Drianus <then.drianus@advotics.com>
2021-11-30 11:46:51 +0700

    add copy to clipboard feature

--- js/generate.js
+++ js/generate.js
@@ -200,3 +200,27 @@ function generate() {
 
   getTimeEntries(defaultWorkspaceId, defaultXAPIKey, dateInput);
 }
+
+function selectElementContents() {
+  var el = document.getElementById("signature_container");
+  var body = document.body,
+    range,
+    sel;
+  if (document.createRange && window.getSelection) {
+    range = document.createRange();
+    sel = window.getSelection();
+    sel.removeAllRanges();
+    try {
+      range.selectNodeContents(el);
+      sel.addRange(range);
+    } catch (e) {
+      range.selectNode(el);
+      sel.addRange(range);
+    }
+  } else if (body.createTextRange) {
+    range = body.createTextRange();
+    range.moveToElementText(el);
+    range.select();
+  }
+  document.execCommand("Copy");
+}
